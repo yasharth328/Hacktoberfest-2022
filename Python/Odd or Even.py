785d4478cf390a04cf38d32cdccdca7001d9292c
Suyash Sachdeva <77083945+suyashsachdeva@users.noreply.github.com>
2021-10-02 11:15:02 +0530

    Update Odd or Even.py

--- Python/Odd or Even.py
+++ Python/Odd or Even.py
@@ -1,4 +1,7 @@
-number = int(input("Which number do you want to check? "))
+try:    
+     number = int(input("Which number do you want to check? "))
+except:
+     print("please enter anumber")
 if number%2 == 0:
      print("This is an Even Number")
 else:
